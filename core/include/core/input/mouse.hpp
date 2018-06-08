7cf18ac50f5dda69d999a5311ebbcb5953007898
Sean Mooney <sean@seanmooney.info>
2018-06-08 23:04:22 +0100

    centralise enums

--- core/include/core/input/mouse.hpp
+++ core/include/core/input/mouse.hpp
@@ -6,21 +6,7 @@
 #include <core/input/enum.hpp>
 
 namespace starlight::core::input {
-enum MouseButtons {
-    MOUSE_BUTTON_1 = 0,
-    MOUSE_BUTTON_2 = 1,
-    MOUSE_BUTTON_3 = 2,
-    MOUSE_BUTTON_4 = 3,
-    MOUSE_BUTTON_5 = 4,
-    MOUSE_BUTTON_6 = 5,
-    MOUSE_BUTTON_7 = 6,
-    MOUSE_BUTTON_8 = 7,
-    MOUSE_BUTTON_NONE = 8,
-    MOUSE_BUTTON_LAST = MOUSE_BUTTON_NONE,
-    MOUSE_BUTTON_LEFT = MOUSE_BUTTON_1,
-    MOUSE_BUTTON_RIGHT = MOUSE_BUTTON_2,
-    MOUSE_BUTTON_MIDDLE = MOUSE_BUTTON_3
-};
+
 class Mouse {
     #include <core/input/enum.hpp>
     friend class InputManager;
